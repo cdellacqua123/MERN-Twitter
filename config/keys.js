ee0dfcac4c0b3f1cb011272166b43649d97d3f2d
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-16 09:23:25 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:NFyP2huToatd3AwC@cluster0.uhdxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+    // secret should be longer; this is just for learning purposes
+}
